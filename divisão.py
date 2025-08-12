2b1a9c273b663a51ec703a17d36f1be06b7b8b52
Caioooolaele <caio.emanuel.garcia@escola.pr.gov.br>
2025-08-12 07:57:41 -0300

    conversão de pdf para png

--- divisão.py
+++ divisão.py
@@ -7,26 +7,26 @@ output_folder = "imagens"
 if not os.path.exists(output_folder):
     os.makedirs(output_folder)
 
-    resolucao_dpi = 300
+resolucao_dpi = 300
 
-    print("convertendo '(pdf_path)' para imagens com (resolucao_dpi) DPI...")
+print(f"Convertendo '{pdf_path}' para imagens com {resolucao_dpi} DPI...")
 
-    try:
-        images =convert_from_path(
-            pdf_path,
-            dpi = resolucao_dpi,
-            output_folder = output_folder,
-            fmt = "png",
-            paths_only = False,
-        );
+try:
+    images = convert_from_path(
+        pdf_path,
+        dpi=resolucao_dpi,
+        output_folder=output_folder,
+        fmt="png",
+        paths_only=False,
+    )
 
-        for i, image in enumerat(images):
-            images_filename = os.path.join(output_folder, f"pagina_enem_{i+1}.png")
-            image.save(images_filename)
-            print("Página {i+1} salva como '{images_filename}'")
+    for i, image in enumerate(images):
+        image_filename = os.path.join(output_folder, f"pagina_enem_{i+1}.png")
+        image.save(image_filename)
+        print(f"Página {i+1} salva como '{image_filename}'")
 
-            print("\nConversão concluída! AS imagens foram salvas na pasta '{output_folder}'.")
+    print(f"\nConversão concluída! As imagens foram salvas na pasta '{output_folder}'.")
 
-            except Exception as e:
-                print(f"Ocorreu um erro durante a conversão:{e}")
-                print("Verifique se o Poppler está instalado corretamente e se o caminho do PDF está correto.")
+except Exception as e:
+    print(f"Ocorreu um erro durante a conversão: {e}")
+    print("Verifique se o Poppler está instalado corretamente e se o caminho do PDF está correto.")
